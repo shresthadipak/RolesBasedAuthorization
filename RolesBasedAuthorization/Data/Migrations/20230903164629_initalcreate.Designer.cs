ed367b0bae08e7b77ad67dc11dad72f9de65307d
Dipak Shrestha <shresthadipak2049@gmail.com>
2023-09-04 17:06:23 +0545

    Feat: Identity Account user login and register using scaffolding

--- RolesBasedAuthorization/Data/Migrations/20230903164629_initalcreate.Designer.cs
+++ RolesBasedAuthorization/Data/Migrations/20230903164629_initalcreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RolesBasedAuthorization.Data;
 
@@ -11,9 +12,11 @@ using RolesBasedAuthorization.Data;
 namespace RolesBasedAuthorization.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230903164629_initalcreate")]
+    partial class initalcreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RolesBasedAuthorization/Data/Migrations/20230903164629_initalcreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RolesBasedAuthorization.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initalcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
